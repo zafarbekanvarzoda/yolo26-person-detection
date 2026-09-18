eaa3cf1119cadd4873771a3ae06de9de7675d577
Zafarbek777 <anikorng@gmail.com>
2026-09-18 10:25:47 +0500

    yolo26n person aniqlash

---
+++ main.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI, UploadFile
+from fastapi.responses import FileResponse
+import shutil
+import os
+
+from video_processor import process_video
+
+
+app = FastAPI()
+
+
+os.makedirs("uploads", exist_ok=True)
+os.makedirs("outputs", exist_ok=True)
+
+
+@app.get("/")
+def root():
+    return {"message": "Hello World"}
+
+
+@app.post("/upload")
+def upload_video(file: UploadFile):
+
+    input_path = f"uploads/{file.filename}"
+    output_path = f"outputs/annotated.mp4"
+
+    with open(input_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    process_video(
+        input_path,
+        output_path
+    )
+
+    return FileResponse(
+        output_path,
+        media_type="video/mp4",
+        filename="annotated.mp4"
+    )
---
+++ video_processor.py
@@ -0,0 +1,62 @@
+from ultralytics import YOLO
+import cv2
+
+
+model = YOLO("yolo26n.pt")
+
+
+def process_video(input_path, output_path):
+
+    cap = cv2.VideoCapture(input_path)
+
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps = cap.get(cv2.CAP_PROP_FPS)
+
+    fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+
+    out = cv2.VideoWriter(
+        output_path,
+        fourcc,
+        fps,
+        (width, height)
+    )
+
+    while True:
+
+        success, frame = cap.read()
+
+        if not success:
+            break
+
+        results = model(frame)
+        result = results[0]
+
+        for box in result.boxes:
+
+            class_id = int(box.cls[0].item())
+            class_name = model.names[class_id]
+
+            if class_name == "person":
+
+                x1, y1, x2, y2 = box.xyxy[0].tolist()
+
+                x1 = int(x1)
+                y1 = int(y1)
+                x2 = int(x2)
+                y2 = int(y2)
+
+                cv2.rectangle(
+                    frame,
+                    (x1, y1),
+                    (x2, y2),
+                    (0, 255, 0),
+                    2
+                )
+
+        out.write(frame)
+
+    cap.release()
+    out.release()
+
+    return output_path
